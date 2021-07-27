872fef3a22d78defee08fbd7e588d43c8a42c8a3
Nisura Indisa <nisura@outlook.com>
2021-07-27 11:26:33 +0530

    Functionally Complete

--- rname.py
+++ rname.py
@@ -1,12 +1,17 @@
 import os
-
+path = "..\Jujutsu Kaisen\\"
+newpath ="JujutsuKaisenRNamed\\"
 season = "01E"
-for filename in os.listdir("..\Jujutsu Kaisen"):
+for filename in os.listdir(path):
     for i in range(1,10):
         if (("("+str(i)[0]+(")")) in filename) or (("_"+str(i)[0]+("_")) in filename):
-            print("Jujusu Kaisen " + season + "0"+ str(i))
+            old = path + filename
+            new = newpath+("Jujusu Kaisen S" + season + "0"+ str(i)+".mp4")
+            os.rename(old,new);
             break
     for i in range(10,25):
         if (("("+str(i)+(")")) in filename) or (("_"+str(i)+("_")) in filename) or ((("Ep"+str(i))) in filename):
-            print("Jujusu Kaisen S" + season + str(i))
+            old = path + filename
+            new = newpath+("Jujusu Kaisen S" + season + str(i)+".mp4")
+            os.rename(old,new);
             break
